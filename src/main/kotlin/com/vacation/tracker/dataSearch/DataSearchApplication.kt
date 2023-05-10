bbdc7b74171799b6f3b443e9c4adb1b6a77b4b49
NenadMilinkovic <nenadmilinkovic95@gmail.com>
2023-05-10 15:10:01 +0200

    App init commit

---
+++ src/main/kotlin/com/vacation/tracker/dataSearch/DataSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.vacation.tracker.dataSearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/vacation/tracker/dataSearch/DataSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vacation.tracker.dataSearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
